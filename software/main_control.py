97b45c97b720004ca0a3d922d98d160f094c4076
Rafoxxa <rakaempfer@uc.cl>
2024-07-25 19:14:08 -0400

    intentando PID donde Carlos

--- software/main_control.py
+++ software/main_control.py
@@ -9,11 +9,11 @@ import sys
 # Configurar la conexión serial
 # puerto: cambia segun el sistema operativo y puerto
 # window: COM3, linux: /dev/ttyUSB0, mac: 
-arduino_port = 'COM3'
+arduino_port = 'COM11'
 baud_rate = 9600 # velocidad de transmisión en baudios
 ser = serial.Serial(arduino_port, baud_rate)
 time.sleep(2)  # esperar a que la conexión serial se establezca
-tipo_control = 1 # 0 manual, 1 automatico
+tipo_control = 1  # 0 manual, 1 automatico
 dt = 0.01
 
 if __name__ == '__main__':
@@ -27,17 +27,18 @@ if __name__ == '__main__':
         control_manual(ser)
     elif tipo_control == 1:
         # inicializar controlador
-        Kp = 1; Ki = 0; Kd = 0
+        Kp = 150; Ki = 0; Kd = 10
         controlador_vel_pos = PIDControl(Kp, Ki, Kd, ser, dt=dt)
         # loop lectura estado y control
         tiempo_referencia = 0
         while True:
+            
             if ser.in_waiting > 0:
                 estado = ser.readline().decode('utf-8').rstrip()
                 # se envian otros mensajes en el debug
-                #print(estado)
-                referencia = [data[tiempo_referencia], velocidad[tiempo_referencia]]
-                referencia = [100, 0]
+                
+                #referencia = [data[tiempo_referencia], velocidad[tiempo_referencia]]
+                referencia = [90, 0]
                 if estado.isdigit():
                     print(estado)
                     controlador_vel_pos.calcular_control(referencia, int(estado))
